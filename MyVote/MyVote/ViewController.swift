0f92c508bb96d2e9a7661332a478fc487f3a6a44
David <43117846+davidria@users.noreply.github.com>
2019-08-25 16:19:33 -0400

    Add Files

---
+++ MyVote/MyVote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyVote
+//
+//  Created by David Araya on 2019-08-25.
+//  Copyright © 2019 ElValle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
