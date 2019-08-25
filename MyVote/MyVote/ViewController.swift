3a9f70e76d21349c993fde638e8db8e6ee3f1d01
David <43117846+davidria@users.noreply.github.com>
2019-08-25 17:08:43 -0400

    Add Button Visuals

--- MyVote/MyVote/ViewController.swift
+++ MyVote/MyVote/ViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var initButton: UIButton!
     @IBAction func initButtonPressed(_ sender: Any) {
         
         print("Button has been pressed")
     }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        let initButton = UIButton(type: .custom)
+        initButton.frame = CGRect(x: 160, y: 100, width: 50, height: 50)
+        initButton.layer.cornerRadius = 0.5 * initButton.bounds.size.width
+        initButton.clipsToBounds = true
+
+        view.addSubview(initButton)
         // Do any additional setup after loading the view.
     }
 
