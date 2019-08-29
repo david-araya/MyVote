5b3e739baf0d55cf1a052240b22d1b2a0aa2f0b3
David <43117846+davidria@users.noreply.github.com>
2019-08-29 07:35:43 -0400

    Add mapview and userlocation

--- MyVote/MyVote/ViewController.swift
+++ MyVote/MyVote/ViewController.swift
@@ -7,21 +7,72 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController, UITextFieldDelegate{
     
     private let locationSource = ["Canada", "U.S.A", "France", "England"]
  
-    @IBOutlet var locationLabel: UILabel!
-    @IBOutlet var locationPicker: UIPickerView!
+    @IBOutlet var mapView: MKMapView!
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var textField: UITextField!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationPicker.dataSource = self
-        locationPicker.delegate = self
+        checkLocationServices()
     }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewUserLocation(){
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled()
+        {
+            //setup our location manager
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            //show alert letting the user know they have to turn it on
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewUserLocation()
+            //Do map stuff
+            break
+        case .denied:
+            //Show alert instructing how to turn on permission
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //User can not change app status
+            // Show alert letting them know whats happening
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            break
+        }
+    }
+        
     @IBAction func initButton(_ sender: Any) {
         
     }
@@ -38,26 +89,13 @@ class ViewController: UIViewController, UITextFieldDelegate{
     }
 }
 
- //view.addSubview(initButton)
- // Do any additional setup after loading the view.
-
-extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
-        return locationSource.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        locationLabel.text = locationSource[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return locationSource[row]
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //
     }
 }
 
